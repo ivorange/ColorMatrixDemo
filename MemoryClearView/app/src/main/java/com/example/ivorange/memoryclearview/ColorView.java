ffeeba56e224100a7a8b7d2fd2ef19b41b3e6add
xiajun <ivorange@126.com>
2015-12-25 16:18:12 +0800

    add new file

---
+++ MemoryClearView/app/src/main/java/com/example/ivorange/memoryclearview/ColorView.java
@@ -0,0 +1,54 @@
+package com.example.ivorange.memoryclearview;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by ivorange on 15/12/25.
+ */
+public class ColorView extends ImageView {
+
+	private Paint myPaint = null;
+	private Bitmap bitmap = null;
+	private ColorMatrix myColorMatrix = null;
+	private float[] colorArray = {1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0};
+
+	public ColorView(Context context, AttributeSet attrs)
+	{
+		super(context, attrs);
+		bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.ttt);
+		invalidate();
+	}
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+		//新建画笔对象
+		myPaint = new Paint();
+		//描画（原始图片）
+		canvas.drawBitmap(bitmap,0, 0, myPaint);
+		//新建颜色矩阵对象
+		myColorMatrix = new ColorMatrix();
+		//设置颜色矩阵的值
+		myColorMatrix.set(colorArray);
+		//设置画笔颜色过滤器
+		myPaint.setColorFilter(new ColorMatrixColorFilter(myColorMatrix));
+		//描画（处理后的图片）
+		canvas.drawBitmap(bitmap,0,0,myPaint);
+		invalidate();
+	}
+	//设置颜色数值
+	public void setColorArray(float[] colorArray){
+		this.colorArray = colorArray;
+	}
+	//设置图片
+	public void setBitmap(Bitmap bitmap){
+		this.bitmap = bitmap;
+	}
+}
